019523715edabf971caf9323789bcd922405dbd6
Canno <838288125@qq.com>
2018-05-20 22:57:35 +0800

    【update】socket 端口 9991

--- admin/src/test/java/com/el/dc/test/NodeInfoTest.java
+++ admin/src/test/java/com/el/dc/test/NodeInfoTest.java
@@ -29,38 +29,38 @@ public class NodeInfoTest {
 
     @Test
     public void test1() throws IOException, InterruptedException {
-            sendData();
+        sendData();
     }
 
-      @Test
+    @Test
     public void test2() throws IOException, InterruptedException {
-            sendData();
+        sendData();
     }
 
-      @Test
+    @Test
     public void test3() throws IOException, InterruptedException {
-            sendData();
+        sendData();
     }
 
-      @Test
+    @Test
     public void test4() throws IOException, InterruptedException {
-            sendData();
+        sendData();
     }
 
-    public static void sendData(){
+    public static void sendData() throws InterruptedException, IOException {
         String data =
 //                " {\"id\":\"20180405\",\"GPS_Lon\":233.234,\"GPS_Lat\":22.11,\"PH\":7.11,\"COD\":22.11,\"DOG\":33.22,\"NHN\":44.33,\"ZS\":55.44,\"TEMP\":66.55}"
 
-        "{\"id\":\"20180405\",\"GPS_Lon\":0.0000,\"GPS_Lat\":0.0000,\"TEM\":20.20,\"PH\":7.02,\"DOG\":0.00,\"NHN\":0.00,\"ZS\":0.00,\"COD\":0.00,\"ERRCODE\":111\"E00001A2\"}";
-                ;
+                "{\"id\":\"20180405\",\"GPS_Lon\":0.0000,\"GPS_Lat\":0.0000,\"TEM\":20.20,\"PH\":7.02,\"DOG\":0.00,\"NHN\":0.00,\"ZS\":0.00,\"COD\":0.00,\"ERRCODE\":111\"E00001A2\"}";
+        ;
         while (true) {
-          //  new SocketRequestUtils("118.178.57.153", 9991).send(data);
+            //  new SocketRequestUtils("118.178.57.153", 9991).send(data);
             new SocketRequestUtils("127.0.0.1", 9991).send(data);
             Thread.sleep(1000);
             System.out.println("success");
             break;
-                "{\"id\":\"20180405\",\"GPS_Lon\":88.0000,\"GPS_Lat\":99.0000,\"TEM\":20.20,\"PH\":7.02,\"DOG\":0.00,\"NHN\":0.00,\"ZS\":0.00,\"COD\":0.00,\"ERRCODE\":\"E00001A2\"}";
 
+        }
     }
 
     final static ObjectMapper objectMapper = new ObjectMapper();
@@ -70,7 +70,7 @@ public class NodeInfoTest {
         String data =
 //                "{\"id\":\"20180405\",\"GPS_Lon\":233.234,\"GPS_Lat\":22.11,\"PH\":7.11,\"COD\":22.11,\"DOG\":33.22,\"NHN\":44.33,\"ZS\":55.44,\"TEMP\":66.55}";
 
-            "{\"id\":\"20180405\",\"GPS_Lon\":0.0000,\"GPS_Lat\":0.0000,\"TEM\":20.20,\"PH\":7.02,\"DOG\":0.00,\"NHN\":0.00,\"ZS\":0.00,\"COD\":0.00,\"ERRCODE\":E00001A2}";
+                "{\"id\":\"20180405\",\"GPS_Lon\":0.0000,\"GPS_Lat\":0.0000,\"TEM\":20.20,\"PH\":7.02,\"DOG\":0.00,\"NHN\":0.00,\"ZS\":0.00,\"COD\":0.00,\"ERRCODE\":E00001A2}";
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         NodeSensorDataReqTO reqTO = objectMapper.readValue(data, NodeSensorDataReqTO.class);
         System.out.println(objectMapper.writeValueAsString(reqTO));
